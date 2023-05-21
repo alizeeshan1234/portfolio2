fb34504b934f7e3367401ec9f9e792bb8585e9b0
alizeeshan1234 <93642091+alizeeshan1234@users.noreply.github.com>
2023-05-21 20:05:41 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+
+
+document.querySelector('#btn1').addEventListener('click', function () {
+    alert("Are you sure...");
+
+
+})
+
+document.querySelector('#btn2').addEventListener('click', function () {
+    $('#div3').toggle('slaw');
+})
+
